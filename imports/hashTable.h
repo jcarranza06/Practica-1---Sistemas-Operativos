78ebd727dd7452c885b0455c2107ee7879f9c065
jcarranza06 <72419038+jcarranza06@users.noreply.github.com>
2023-03-31 22:58:34 -0500

    se crea la ultima columna para lista enlazada de elementos con mismo id source, destination en el csv junto con las modificaciones en crearArchivoBusqueda

--- imports/hashTable.h
+++ imports/hashTable.h
@@ -1,15 +1,17 @@
-// include stdio.h stdlib.h
+// include stdio.h stdlib.h stdbool.h
 #define TABLE_SIZE 1000000 // Tamaño de la tabla hash
 
-typedef struct HashNode {
+typedef struct HashNode
+{
     int key;
     int value;
-    struct HashNode* next; // Puntero al siguiente nodo en caso de colisión
+    struct HashNode *next; // Puntero al siguiente nodo en caso de colisión
 } HashNode;
 
-HashNode* hashTable[TABLE_SIZE]; // La tabla hash es un arreglo de punteros a nodos
+HashNode *hashTable[TABLE_SIZE]; // La tabla hash es un arreglo de punteros a nodos
 
-int hashFunction(int key) {
+int hashFunction(int key)
+{
     // La función hash simplemente devuelve el residuo de la división del key por el tamaño de la tabla
     unsigned int hash = key;
 
@@ -24,28 +26,55 @@ int hashFunction(int key) {
     return hash % TABLE_SIZE;
 }
 
-void insert(int key, int value) {
-    int index = hashFunction(key); // Calcula el índice de la tabla para el key dado
-    HashNode* newNode = (HashNode*)malloc(sizeof(HashNode)); // Crea un nuevo nodo
+void insert(int key, int value)
+{
+    int index = hashFunction(key);                            // Calcula el índice de la tabla para el key dado
+    HashNode *newNode = (HashNode *)malloc(sizeof(HashNode)); // Crea un nuevo nodo
     newNode->key = key;
     newNode->value = value;
     newNode->next = NULL;
-    if (hashTable[index] == NULL) { // Si el índice de la tabla está vacío
+    bool continuate = true;
+    if (hashTable[index] == NULL)
+    {                               // Si el índice de la tabla está vacío
         hashTable[index] = newNode; // Asigna el nuevo nodo como el primer elemento en ese índice
-    } else { // Si el índice de la tabla ya tiene elementos
-        HashNode* currentNode = hashTable[index]; // Obtiene el primer nodo en ese índice
-        while (currentNode->next != NULL) { // Avanza hasta el último nodo en la lista
-            currentNode = currentNode->next;
+    }
+    else
+    {                                             // Si el índice de la tabla ya tiene elementos
+        HashNode *currentNode = hashTable[index]; // Obtiene el primer nodo en ese índice
+        if (currentNode->key == key) // en caso de que la llave esté en la tabla, no se realiza la busqueda en la lista
+        {
+            continuate = false;
         }
-        currentNode->next = newNode; // Agrega el nuevo nodo al final de la lista
+        while (currentNode->next != NULL && continuate)
+        { // Avanza hasta el último nodo en la lista
+            if (currentNode->key == key)
+            {
+                continuate = false; // en caso de encontar la llave en la tabla, detiene la busqueda
+            }
+            else
+            {
+                currentNode = currentNode->next;
+            }
+        }
+        if (currentNode->key == key)
+        {
+            currentNode->value = value; // sí se tiene la llave en la tabla solo se actualiza el valor 
+        }
+        else
+        {
+            currentNode->next = newNode; // Agrega el nuevo nodo al final de la lista
+        }
     }
 }
 
-int* search(int key) {
-    int index = hashFunction(key); // Calcula el índice de la tabla para el key dado
-    HashNode* node = hashTable[index]; // Obtiene el primer nodo en ese índice
-    while (node != NULL) { // Recorre la lista de nodos en ese índice
-        if (node->key == key) { // Si el nodo tiene la key buscada
+int *search(int key)
+{
+    int index = hashFunction(key);     // Calcula el índice de la tabla para el key dado
+    HashNode *node = hashTable[index]; // Obtiene el primer nodo en ese índice
+    while (node != NULL)
+    { // Recorre la lista de nodos en ese índice
+        if (node->key == key)
+        {                          // Si el nodo tiene la key buscada
             return &(node->value); // Retorna un puntero al valor asociado a esa key
         }
         node = node->next; // Avanza al siguiente nodo en la lista
@@ -53,45 +82,54 @@ int* search(int key) {
     return NULL; // Retorna NULL si la key no se encontró en la tabla hash
 }
 
-void destroy() {
-    for (int i = 0; i < TABLE_SIZE; i++) { // Recorre todos los índices de la tabla
-        HashNode* node = hashTable[i]; // Obtiene el primer nodo en ese índice
-        while (node != NULL) { // Recorre la lista de nodos en ese índice
-            HashNode* nextNode = node->next; // Obtiene el siguiente nodo en la lista
-            free(node); // Libera la memoria del nodo actual
-            node = nextNode; // Avanza al siguiente nodo en la lista
+void destroy()
+{
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {                                  // Recorre todos los índices de la tabla
+        HashNode *node = hashTable[i]; // Obtiene el primer nodo en ese índice
+        while (node != NULL)
+        {                                    // Recorre la lista de nodos en ese índice
+            HashNode *nextNode = node->next; // Obtiene el siguiente nodo en la lista
+            free(node);                      // Libera la memoria del nodo actual
+            node = nextNode;                 // Avanza al siguiente nodo en la lista
         }
         hashTable[i] = NULL; // Asigna NULL al puntero en ese índice
     }
 }
 
-void saveHashTable(char* filename) {
-    FILE* fp = fopen(filename, "wb"); // Abre el archivo para escritura binaria
-    if (fp != NULL) { // Si el archivo se abrió correctamente
-        for (int i = 0; i < TABLE_SIZE; i++) { // Recorre todos los índices de la tabla
-            HashNode* node = hashTable[i]; // Obtiene el primer nodo en ese índice
-            while (node != NULL) { // Recorre la lista de nodos en ese índice
-                fwrite(&(node->key), sizeof(int), 1, fp); // Escribe la key en el archivo
+void saveHashTable(char *filename)
+{
+    FILE *fp = fopen(filename, "wb"); // Abre el archivo para escritura binaria
+    if (fp != NULL)
+    { // Si el archivo se abrió correctamente
+        for (int i = 0; i < TABLE_SIZE; i++)
+        {                                  // Recorre todos los índices de la tabla
+            HashNode *node = hashTable[i]; // Obtiene el primer nodo en ese índice
+            while (node != NULL)
+            {                                               // Recorre la lista de nodos en ese índice
+                fwrite(&(node->key), sizeof(int), 1, fp);   // Escribe la key en el archivo
                 fwrite(&(node->value), sizeof(int), 1, fp); // Escribe el valor en el archivo
-                node = node->next; // Avanza al siguiente nodo en la lista
+                node = node->next;                          // Avanza al siguiente nodo en la lista
             }
         }
         fclose(fp); // Cierra el archivo
     }
 }
 
-void loadHashTable(char* filename) {
-    FILE* fp = fopen(filename, "rb"); // Abre el archivo para lectura binaria
-    if (fp != NULL) { // Si el archivo se abrió correctamente
+void loadHashTable(char *filename)
+{
+    FILE *fp = fopen(filename, "rb"); // Abre el archivo para lectura binaria
+    if (fp != NULL)
+    {              // Si el archivo se abrió correctamente
         destroy(); // Borra todos los elementos de la tabla hash actual
         int key, value;
-        while (fread(&key, sizeof(int), 1, fp) && fread(&value, sizeof(int), 1, fp)) { // Lee una key y un valor del archivo
+        while (fread(&key, sizeof(int), 1, fp) && fread(&value, sizeof(int), 1, fp))
+        {                       // Lee una key y un valor del archivo
             insert(key, value); // Agrega el par key-value a la tabla hash
         }
         fclose(fp); // Cierra el archivo
     }
 }
-
 
 /*int main() {
     // Inserta algunos pares key-value en la tabla hash
@@ -101,11 +139,11 @@ void loadHashTable(char* filename) {
     insert(232146, 20);
     insert(963258, 10);
     insert(116011602, 35);
-    
+
     saveHashTable("tablahash.a");*/
-    /*loadHashTable("tablahash.a");
+/*loadHashTable("tablahash.a");
 
-    printf("resultado: %d\n", *search(116011602));
+printf("resultado: %d\n", *search(116011602));
 
-    return 0;
+return 0;
 }*/
--- src/crearArchivoBusqueda.c
+++ src/crearArchivoBusqueda.c
@@ -5,6 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+//se incluye la tabla hash 
+#include "../imports/hashTable.h"
 
 #define MAX_LINE_LENGTH 1000 // Longitud máxima de una línea del archivo
 
@@ -23,27 +27,36 @@ int main()
     fgets(line, MAX_LINE_LENGTH, input_file);
     fputs(line, output_file); 
     int i = 0;
+
+    /*insert(963258, 10);
+    insert(116011602, 35);
+    insert(116011602, 9);
+    insert(116011602, 32);
+    printf("resultado: %d\n", *search(116011602));*/
     while (fgets(line, MAX_LINE_LENGTH, input_file))
     { // Leer cada línea del archivo
-        i++;
+        
         if(i%200000==0){
-            printf("%d\n",i);
+            printf("%d\n",i);// para ver el proreso en consola
         }
         char *field;
         int field_count = 0;
         char modified_line[MAX_LINE_LENGTH] = ""; // Línea modificada
-
+        char modified_identifier[MAX_LINE_LENGTH] = ""; // linea de identificador 
+        char identifier[16] = "";
         // Separar los campos por comas y convertirlos al formato deseado
         for (field = strtok(line, ","); field != NULL; field = strtok(NULL, ","))
         {
             field_count++;
             char modified_field[8] = ""; // Campo modificado
-
+            
             switch (field_count)
             {
             case 1:
             case 2:
                 sprintf(modified_field, "%04d", atoi(field)); // Rellenar con 0 a la izquierda hasta 4 dígitos
+                sprintf(identifier, "%07d", atoi(field)); // Rellenar con 0el identificador
+                strcat(modified_identifier, identifier); // agregado a la linea identificador
                 break;
             case 3:
                 sprintf(modified_field, "%02d", atoi(field)); // Rellenar con 0 a la izquierda hasta 2 dígitos
@@ -59,12 +72,24 @@ int main()
             strcat(modified_line, ",");            // Agregar coma después de cada campo
         }
 
+        // se le asigna una lista enlazada en la que se relaciona cada par de sourceid, dstid
+        char modified_field[8] = ""; // Campo modificado
+        if(search(atoi(modified_identifier))==NULL){// en caso de que no se haya guardado antes 
+            sprintf(modified_field, "%07d", -1); // en caso ed ser el primero que aparece (o el ultimo de la lista) se le asigna -1
+        }else{ //en caso de estar ya en la tabla
+            sprintf(modified_field, "%07d", *search(atoi(modified_identifier))); // se guarda el anterior indice del csv relacionado a la dupla sourceid,dstid
+        }
+        insert(atoi(modified_identifier), i);// se inserta en el mapa la llave identificador, indice, identificador: es la concat de dos enteros de 7 digitos ej 00001590000023. indice es el indice respecto a los elem del csv
+        strcat(modified_line, modified_field); // Agregar campo modificado a la línea modificada
+        strcat(modified_line, ",");
+
         modified_line[strlen(modified_line) - 1] = '\n'; // Reemplazar última coma con salto de línea
         fputs(modified_line, output_file);               // Escribir línea modificada en el archivo modificado
+        i++;
     }
 
     fclose(input_file); // Cerrar archivos
     fclose(output_file);
-
+    printf("Finalizado aparentemente bien \n");
     return 0;
 }
